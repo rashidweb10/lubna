ade3b377040237d4715a08dc5c90c497688bd2e3
Muhammed rashid <rashidk.developer@gmail.com>
2026-02-02 22:18:32 +0530

    Serivces backend page dynamic - 90%

--- resources/views/backend/pages/edit-layouts/services.blade.php
+++ resources/views/backend/pages/edit-layouts/services.blade.php
@@ -2,10 +2,7 @@
 @php
 $banner_images = $pageData->meta->where('meta_key', 'banner_images')->first()->meta_value ?? '';
 
-$about_title = $pageData->meta->where('meta_key', 'about_title')->first()->meta_value ?? '';
-$about_description = $pageData->meta->where('meta_key', 'about_description')->first()->meta_value ?? '';
-
-$faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->meta_value ?? '[]', true);
+$services = json_decode($pageData->meta->where('meta_key', 'services')->first()->meta_value ?? '[]', true);
 
 @endphp
 
@@ -32,30 +29,15 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
 <div class="row">
     <div class="col-md-12">
         <hr>
-        <h4 class="text-primary">About Section</h4>
-    </div>        
-    <div class="col-md-12 form-group mb-2">
-        <label for="name" class="form-label">Title<span class="text-danger">*</span></label>
-        <input class="form-control" value="{{$about_title}}" name="meta[about_title]" type="text" required>
-    </div>        
-    <div class="col-md-12 form-group mb-2">
-        <label for="content" class="form-label">Description <span class="text-danger">*</span></label>
-        <textarea name="meta[about_description]" class="form-control text-editor" rows="4" required>{{$about_description}}</textarea>
-    </div>     
-</div> 
-
-<div class="row">
-    <div class="col-md-12">
-        <hr>
-        <h4 class="text-primary">Faculty</h4>
+        <h4 class="text-primary">services</h4>
     </div>    
     <div class="quicklinks-target">
-        @if(isset($faculty['itration']) && is_array($faculty['itration']))
-            @foreach($faculty['itration'] as $index => $itration)
+        @if(isset($services['itration']) && is_array($services['itration']))
+            @foreach($services['itration'] as $index => $itration)
                 <div class="row remove-parent">
                     <div class="col-md-12">
-                        <label for="name" class="form-label">Faculty <span class="text-danger">*</span></label>
-                        <input value="{{ $index }}" name="meta[faculty][itration][]" type="hidden" required>
+                        <label for="name" class="form-label">services <span class="text-danger">*</span></label>
+                        <input value="{{ $index }}" name="meta[services][itration][]" type="hidden" required>
                     </div> 
                     <div class="col-md">
                         <div class="form-group mb-2">
@@ -65,9 +47,9 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
                                 </div>
                                 <div class="form-control file-amount">{{ __('Choose File') }}</div>
                                 <input type="hidden" 
-                                    name="meta[faculty][image][]" 
+                                    name="meta[services][image][]" 
                                     class="selected-files" 
-                                    value="{{ $faculty['image'][$index] ?? '' }}" 
+                                    value="{{ $services['image'][$index] ?? '' }}" 
                                     required>
                             </div>
                             <div class="file-preview box sm"></div>
@@ -75,8 +57,8 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
                     </div>
                     <div class="col-md">
                         <div class="form-group mb-2">
-                            <input value="{{ $faculty['name'][$index] ?? '' }}" 
-                                name="meta[faculty][name][]" 
+                            <input value="{{ $services['name'][$index] ?? '' }}" 
+                                name="meta[services][name][]" 
                                 type="text" 
                                 class="form-control" 
                                 minlength="3" 
@@ -88,10 +70,10 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
                     <div class="col-md">
                         <div class="form-group mb-2">
                             <textarea  
-                                name="meta[faculty][description][]" 
-                                class="form-control" 
+                                name="meta[services][description][]" 
+                                class="form-control text-editor" 
                                 rows="1" 
-                                required>{{ $faculty['description'][$index] ?? '' }}</textarea>
+                                required>{{ $services['description'][$index] ?? '' }}</textarea>
                         </div>
                     </div>                                       
                     <div class="col-md-auto">
@@ -110,8 +92,8 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
         data-content='
             <div class="row remove-parent">
                 <div class="col-md-12">
-                    <label for="name" class="form-label">Faculty <span class="text-danger">*</span></label>
-                    <input value="data" name="meta[faculty][itration][]" type="hidden" required>
+                    <label for="name" class="form-label">services <span class="text-danger">*</span></label>
+                    <input value="data" name="meta[services][itration][]" type="hidden" required>
                 </div> 
                 <div class="col-md">
                     <div class="form-group mb-2">
@@ -120,19 +102,19 @@ $faculty = json_decode($pageData->meta->where('meta_key', 'faculty')->first()->m
                                 <div class="input-group-text bg-soft-secondary font-weight-medium">{{ __('Browse') }}</div>
                             </div>
                             <div class="form-control file-amount">{{ __('Choose File') }}</div>
-                            <input type="hidden" name="meta[faculty][image][]" class="selected-files" required>
+                            <input type="hidden" name="meta[services][image][]" class="selected-files" required>
                         </div>
                         <div class="file-preview box sm"></div>
                     </div>
                 </div> 
                 <div class="col-md">
                     <div class="form-group mb-2">
-                        <input value="" name="meta[faculty][name][]" type="text" class="form-control" minlength="3" maxlength="200" placeholder="Enter name" required>
+                        <input value="" name="meta[services][name][]" type="text" class="form-control" minlength="3" maxlength="200" placeholder="Enter name" required>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-group mb-2">
-                        <textarea value="" name="meta[faculty][description][]" class="form-control" rows="1" placeholder="Enter description" required></textarea>
+                        <textarea value="" name="meta[services][description][]" class="form-control text-editor" rows="1" placeholder="Enter description" required></textarea>
                     </div>
                 </div>                              
                 <div class="col-md-auto">
