838d0920561e6d2d4ac355fbe3f368454950cf01
Muhammed rashid <rashidk.developer@gmail.com>
2026-02-13 21:14:03 +0530

    some fixes in backend

--- resources/views/backend/pages/edit-layouts/about.blade.php
+++ resources/views/backend/pages/edit-layouts/about.blade.php
@@ -15,26 +15,6 @@ $about_image2 = $pageData->meta->where('meta_key', 'about_image2')->first()->met
 <div class="row">
     <div class="col-md-12">
         <hr>
-        <h4 class="text-primary">Breadcrumb Section</h4>
-    </div>      
-    <div class="col-md-12">
-        <label for="name" class="form-label">Breadcrumb <span class="text-danger">*</span></label>
-        <div class="form-group mb-2">
-            <div class="input-group" data-toggle="aizuploader" data-type="image" data-multiple="false">
-                <div class="input-group-prepend">
-                    <div class="input-group-text bg-soft-secondary font-weight-medium">{{ __('Browse') }}</div>
-                </div>
-                <div class="form-control file-amount">{{ __('Choose File') }}</div>
-                <input value="{{$banner_images}}" type="hidden" name="meta[banner_images]" class="selected-files" required>
-            </div>
-            <div class="file-preview box sm"></div>
-        </div>
-    </div>    
-</div>
-
-<div class="row">
-    <div class="col-md-12">
-        <hr>
         <h4 class="text-primary">About Section</h4>
     </div>        
     <div class="col-md-6 form-group mb-2">
